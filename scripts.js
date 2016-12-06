35f6b4133130f58e7a7643f502d88228a3f265d3
Charles Dunn <dunnc1551@gmail.com>
2016-12-05 21:03:07 -0700

    Add function to generate random number

---
+++ scripts.js
@@ -0,0 +1,16 @@
+// Generate Random Number
+function getRandomNum() {
+  var randomNum = Math.floor((Math.random() * 100) + 1);
+}
+var secretNum = parseInt(randomNum);
+
+// Guess Button Click Event
+guessButton.addEventListener('click', function(){
+  var guessInput = document.getElementById('user-guess');
+  var guessButton = document.getElementById('guess-btn');
+  var userInput = guessInput.value;
+
+  if (userInput === ) {
+
+  }
+})
