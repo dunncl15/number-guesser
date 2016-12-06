067ee126cfa7d8082b48acf300644970afd5f09f
Charles Dunn <dunnc1551@gmail.com>
2016-12-05 21:59:16 -0700

    Add if else statment for user inputs. Add query selector for displaying last guess

--- scripts.js
+++ scripts.js
@@ -1,16 +1,25 @@
 // Generate Random Number
-function getRandomNum() {
-  var randomNum = Math.floor((Math.random() * 100) + 1);
-}
+// var getRandomNum = function() {
+//   Math.floor((Math.random() * 100) + 1);
+// }
+var randomNum = Math.floor((Math.random() * 100) + 1);
 var secretNum = parseInt(randomNum);
 
 // Guess Button Click Event
+var guessInput = document.getElementById('user-guess');
+var guessButton = document.getElementById('guess-btn');
+
 guessButton.addEventListener('click', function(){
-  var guessInput = document.getElementById('user-guess');
-  var guessButton = document.getElementById('guess-btn');
   var userInput = guessInput.value;
+  document.querySelector('.last-guess').innerText = userInput;
+  
+  console.log(userInput, secretNum);
 
-  if (userInput === ) {
-
+  if (userInput === secretNum) {
+    document.querySelector('.result').innerText = "Boom!";
+  } else if (userInput > secretNum) {
+    document.querySelector('.result').innerText = "That is too high";
+  } else {
+    document.querySelector('.result').innerText = "That is too low";
   }
-})
+});
